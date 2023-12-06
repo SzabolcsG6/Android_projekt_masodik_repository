618c307b4f7469cced0d8eb7ca22ab37bf0d79ce
Szaby G <64457177+SzabolcsG6@users.noreply.github.com>
2023-12-06 21:58:07 +0200

    RecipesFragment.kt added                 Recipe reading in progress

--- app/src/main/java/com/tasty/recipesapp/ui/recipe/RecipesFragment.kt
+++ app/src/main/java/com/tasty/recipesapp/ui/recipe/RecipesFragment.kt
@@ -1,41 +1,41 @@
-package com.tasty.recipesapp.ui.recipe
-
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.viewModels
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.tasty.recipesapp.R
-import com.tasty.recipesapp.models.RecipeListViewModel
-
-
 
 class RecipesFragment : Fragment() {
 
-    private val recipeViewModel : RecipeListViewModel by viewModels()
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        //requireActivity().title = "Recipes"
-
-        recipeViewModel.loadInstructionData(requireContext())
-        recipeViewModel.instructionModels.observe(viewLifecycleOwner) {  instructions ->
-            for (instructionModel in instructions) {
-                Log.d("Instructions", instructionModel.toString())
-            }
-        }
-    }
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var recipeAdapter: RecipeAdapter
+    private lateinit var recipeViewModel: RecipeViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.recipes_fragment, container, false)
-    }//lehessen hozzaadni recepteket, listazni a recepteket
+        val view = inflater.inflate(R.layout.recipes_fragment, container, false)
+
+        recyclerView = view.findViewById(R.id.recyclerView)
+        recipeAdapter = RecipeAdapter(/* pass recipe list here */)
+        recyclerView.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = recipeAdapter
+        }
+
+        // Initialize ViewModel and fetch recipes
+        recipeViewModel = ViewModelProvider(this).get(RecipeViewModel::class.java)
+        recipeViewModel.readAllRecipes(requireContext())
+
+        // Observe the LiveData containing the recipe list
+        recipeViewModel.recipeList.observe(viewLifecycleOwner) { recipes ->
+            recipeAdapter.submitList(recipes)
+        }
+
+        return view
+    }
 }
