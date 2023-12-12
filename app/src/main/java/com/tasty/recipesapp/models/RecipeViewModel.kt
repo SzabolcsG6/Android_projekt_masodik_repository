afc263450cd7eb779949a6b3c350159409d26be0
Szaby G <64457177+SzabolcsG6@users.noreply.github.com>
2023-12-12 17:05:59 +0200

    recipeDao problems

--- app/src/main/java/com/tasty/recipesapp/models/RecipeViewModel.kt
+++ app/src/main/java/com/tasty/recipesapp/models/RecipeViewModel.kt
@@ -6,42 +6,43 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import com.tasty.recipesapp.dtos.Recipe
 import java.io.IOException
 
 class RecipeViewModel : ViewModel() {
-//    private val _recipeList = MutableLiveData<List<Recipe>>() // Define LiveData for recipes
-//    val recipeList: LiveData<List<Recipe>> get() = _recipeList // Expose LiveData
-//
-//    fun readAllRecipes(context: Context) {
-//        val jsonString = loadJsonFromAssets(context, "all_recipes.json") // Load JSON file
-//        val recipeList = parseJsonToRecipes(jsonString) // Parse JSON to list of Recipe objects
-//        _recipeList.value = recipeList // Set the value of LiveData
-//    }
-//
-//    private fun loadJsonFromAssets(context: Context, fileName: String): String {
-//        return try {
-//            val inputStream = context.assets.open(fileName)
-//            val size = inputStream.available()
-//            val buffer = ByteArray(size)
-//            inputStream.read(buffer)
-//            inputStream.close()
-//            String(buffer)
-//        } catch (ioException: IOException) {
-//            ioException.printStackTrace()
-//            ""
-//        }
-//    }
-//
-//    private fun parseJsonToRecipes(jsonString: String): List<Recipe> {
-//        val gson = Gson()
-//        val listType = object : TypeToken<List<Recipe>>() {}.type
-//        return gson.fromJson(jsonString, listType)
-//    }
-//
-//    // Filter recipes based on the provided type
-//    fun filterRecipesByType(type: String) {
-//        val currentRecipes = _recipeList.value.orEmpty()
-//        val filteredRecipes = currentRecipes.filter { it.type == type }
-//        _recipeList.value = filteredRecipes
-//    }
+    private val _recipeList = MutableLiveData<List<Recipe>>() // Define LiveData for recipes
+    val recipeList: LiveData<List<Recipe>> get() = _recipeList // Expose LiveData
+
+    fun readAllRecipes(context: Context) {
+        val jsonString = loadJsonFromAssets(context, "all_recipes.json") // Load JSON file
+        val recipeList = parseJsonToRecipes(jsonString) // Parse JSON to list of Recipe objects
+        _recipeList.value = recipeList // Set the value of LiveData
+    }
+
+    private fun loadJsonFromAssets(context: Context, fileName: String): String {
+        return try {
+            val inputStream = context.assets.open(fileName)
+            val size = inputStream.available()
+            val buffer = ByteArray(size)
+            inputStream.read(buffer)
+            inputStream.close()
+            String(buffer)
+        } catch (ioException: IOException) {
+            ioException.printStackTrace()
+            ""
+        }
+    }
+
+    private fun parseJsonToRecipes(jsonString: String): List<Recipe> {
+        val gson = Gson()
+        val listType = object : TypeToken<List<Recipe>>() {}.type
+        return gson.fromJson(jsonString, listType)
+    }
+
+    // Filter recipes based on the provided type
+    fun filterRecipesByType(type: String) {
+        val currentRecipes = _recipeList.value.orEmpty()
+        val filteredRecipes = currentRecipes.filter { it.type == type }
+        _recipeList.value = filteredRecipes
+    }
 }
--- app/src/main/java/com/tasty/recipesapp/ui/recipe/RecipesFragment.kt
+++ app/src/main/java/com/tasty/recipesapp/ui/recipe/RecipesFragment.kt
@@ -1,22 +1,65 @@
-//import android.os.Build.VERSION_CODES.R
-//import android.os.Bundle
-//import android.view.LayoutInflater
-//import android.view.View
-//import android.view.ViewGroup
-//import androidx.core.os.bundleOf
-//import androidx.fragment.app.Fragment
-//import androidx.lifecycle.ViewModelProvider
-//import androidx.navigation.fragment.findNavController
-//import androidx.recyclerview.widget.LinearLayoutManager
-//import androidx.recyclerview.widget.RecyclerView
-//import com.tasty.recipesapp.R
-//import com.tasty.recipesapp.models.RecipeViewModel
-//
+import android.os.Build.VERSION_CODES.R
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.tasty.recipesapp.R
+import com.tasty.recipesapp.models.RecipeViewModel
+
+
+class RecipesFragment : Fragment(), RecipeAdapter.OnItemClickListener {
+
+    private lateinit var binding: FragmentRecipesBinding
+    private val recipeAdapter = RecipeAdapter(this)
+    private val recipeViewModel: RecipeViewModel by lazy {
+        ViewModelProvider(this).get(RecipeViewModel::class.java)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentRecipesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.recyclerViewRecipes.adapter = recipeAdapter
+
+        recipeViewModel.readAllRecipes(requireContext())
+        recipeViewModel.recipeList.observe(viewLifecycleOwner) {
+            recipeAdapter.setData(it)
+        }
+
+        binding.newRecipeButton.setOnClickListener {
+            findNavController().navigate(R.id.action_recipesFragment_to_addEditRecipeFragment)
+        }
+    }
+
+    override fun onItemClick(id: Int) {
+        findNavController().navigate(R.id.action_recipesFragment_to_recipeDetailFragment, bundleOf("recipeId" to id))
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        // Release the binding when the fragment's view is destroyed to avoid memory leaks
+        binding = null
+    }
+}
+
 //class RecipesFragment : Fragment(), RecipeAdapter.OnItemClickListener {
 //
 //    // itt peldanyositjuk az adaptert
 //    private val recipeAdapter = RecipeAdapter(this)
-//    private val binding get() = _binding!!
+//    private val binding get() = binding
 //    override fun onCreate(savedInstanceState: Bundle?) {
 //        super.onCreate(savedInstanceState)
 //    }
@@ -26,42 +69,42 @@
 //
 //        binding.recyclerViewRecipes.adapter = recipeAdapter
 //
-//        // ez meg az allomanybol valo olvasas meghivasa
-////        RecipeViewModel.readAllRecipes(requireContext())
-////        RecipeViewModel.recipeList.observe(viewLifecycleOwner) {
-////            // ha valtozik az adat update-et hivunk
-////            recipeAdapter.setData(it)
-////        super.onViewCreated(view, savedInstanceState)
-////
-////        binding.recipesRecyclerView.adapter = recipeAdapter
-////
-////        // Find the "New" button and set its click listener
-////        binding.newRecipeButton.setOnClickListener {
-////            // Navigate to the fragment for adding a new recipe
-////            findNavController().navigate(R.id.action_recipesFragment_to_addEditRecipeFragment)
-////        }
-////        binding.recipesRecyclerView.adapter = recipeAdapter
-////
-////        // Find the "New" button and set its click listener
-////        binding.newRecipeButton.setOnClickListener {
-////            // Navigate to the NewRecipeFragment
-////            findNavController().navigate(R.id.action_recipesFragment_to_newRecipeFragment)
-////        }
-////        }
-////    }
-////
-////
-////    override fun onCreateView(
-////        inflater: LayoutInflater, container: ViewGroup?,
-////        savedInstanceState: Bundle?
-////    ): View {
-////        // Inflate the layout for this fragment
-////        _binding = FragmentRecipesBinding.inflate(inflater, container, false)
-////        return binding.root
-////    }
-////
+//         //ez meg az allomanybol valo olvasas meghivasa
+//        RecipeViewModel.readAllRecipes(requireContext())
+//        RecipeViewModel.recipeList.observe(viewLifecycleOwner) {
+//            // ha valtozik az adat update-et hivunk
+//            recipeAdapter.setData(it)
+//        super.onViewCreated(view, savedInstanceState)
+//
+//        binding.recipesRecyclerView.adapter = recipeAdapter
+//
+//        // Find the "New" button and set its click listener
+//        binding.newRecipeButton.setOnClickListener {
+//            // Navigate to the fragment for adding a new recipe
+//            findNavController().navigate(R.id.action_recipesFragment_to_addEditRecipeFragment)
+//        }
+//        binding.recipesRecyclerView.adapter = recipeAdapter
+//
+//        // Find the "New" button and set its click listener
+//        binding.newRecipeButton.setOnClickListener {
+//            // Navigate to the NewRecipeFragment
+//            findNavController().navigate(R.id.action_recipesFragment_to_newRecipeFragment)
+//        }
+//        }
+//    }
+//
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View {
+//        // Inflate the layout for this fragment
+//        _binding = FragmentRecipesBinding.inflate(inflater, container, false)
+//        return binding.root
+//    }
+//
 ////    // es itt kezeljuk le a click event-et, atmegyunk mas fragment-re
-////    override fun onItemClick(id: Int) {
-////        findNavController().navigate(R.id.action_recipesFragment_to_recipeDetailFragment,  bundleOf("recipeId" to id))
-////    }
+//    override fun onItemClick(id: Int) {
+//        findNavController().navigate(R.id.action_recipesFragment_to_recipeDetailFragment,  bundleOf("recipeId" to id))
+//    }
 //}}
