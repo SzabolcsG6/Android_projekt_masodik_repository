f0ef059d9511bc223dcf0a65c7c3c4584bae9b94
Szaby G <64457177+SzabolcsG6@users.noreply.github.com>
2023-12-12 13:30:27 +0200

    Android studio works, mostly

--- app/src/main/java/com/tasty/recipesapp/data/RecipeAdapter.kt
+++ app/src/main/java/com/tasty/recipesapp/data/RecipeAdapter.kt
@@ -3,55 +3,55 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.tasty.recipesapp.models.RecipeViewModel
 
-// paramaterkent megkapja a listener-t, amit a fragment-ben kezelunk le
-//class RecipeAdapter(private var listener: OnItemClickListener) : RecyclerView.Adapter<RecipeAdapter.RecipeViewHolder>() {
+ paramaterkent megkapja a listener-t, amit a fragment-ben kezelunk le
+class RecipeAdapter(private var listener: OnItemClickListener) : RecyclerView.Adapter<RecipeAdapter.RecipeViewHolder>() {
 
-    
-   // interface OnItemClickListener {
-    //    fun onItemClick(id: Int)
-   // }
-	
-//    inner class RecipeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
-//        //implementalja a View.OnClickListener-t
-//		//Vigyazat!!! Ez nem ugyanaz, mint az altalunk megadott interface, ez az alap OnClick
-//
-//		//....
-//
-//
-//        init {
-//			//listener beallitasa
-//            itemView.setOnClickListener(this)
-//        }
-//
-//		// implementacio, ahol megadjuk a mi sajat listener-unknek, hogy mi lesz a recipe ID.
-//         override fun onClick(view: View) {
-//             val position = adapterPosition
-//             if (position != RecyclerView.NO_POSITION) {
-//				//innen fogja tudni, hogy mi az id
-//                 listener.onItemClick(recipes[position].id)
-//             }
-//         }
-//    }
-//
-//	fun setData(list: List<RecipeViewModel>?) {
-//        recipes.clear()
-//        recipes.addAll(list!!)
-//        notifyDataSetChanged()
-//    }
-//
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeViewHolder {
-//        TODO("Not yet implemented")
-//    }
-//
-//    override fun getItemCount(): Int {
-//        TODO("Not yet implemented")
-//    }
-//
-//    override fun onBindViewHolder(holder: RecipeViewHolder, position: Int) {
-//        TODO("Not yet implemented")
-//    }
-//
-//    //.....
-    
-//}
+
+    interface OnItemClickListener {
+        fun onItemClick(id: Int)
+    }
+
+    inner class RecipeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+        //implementalja a View.OnClickListener-t
+		//Vigyazat!!! Ez nem ugyanaz, mint az altalunk megadott interface, ez az alap OnClick
+
+		//....
+
+
+        init {
+			//listener beallitasa
+            itemView.setOnClickListener(this)
+        }
+
+		// implementacio, ahol megadjuk a mi sajat listener-unknek, hogy mi lesz a recipe ID.
+         override fun onClick(view: View) {
+             val position = adapterPosition
+             if (position != RecyclerView.NO_POSITION) {
+				//innen fogja tudni, hogy mi az id
+                 listener.onItemClick(recipes[position].id)
+             }
+         }
+    }
+
+	fun setData(list: List<RecipeViewModel>?) {
+        recipes.clear()
+        recipes.addAll(list!!)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecipeViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    //.....
+
+}
 
--- app/src/main/java/com/tasty/recipesapp/models/RecipeListViewModel.kt
+++ app/src/main/java/com/tasty/recipesapp/models/RecipeListViewModel.kt
@@ -1,32 +1,32 @@
-//package com.tasty.recipesapp.models
-//
-//import android.content.Context
-//import androidx.lifecycle.LiveData
-//import androidx.lifecycle.MutableLiveData
-//import androidx.lifecycle.ViewModel
-//import com.tasty.recipesapp.dtos.recipeDto
-//import com.tasty.recipesapp.providers.RepositoryProvider
-//
-//class RecipeListViewModel : ViewModel() {
-//
-//    // LiveData to hold the list of InstructionModels
-//    // This should be changed to recipes
-//    private val _instructionModels = MutableLiveData<List<InstructionModel>>()
-//    val instructionModels: LiveData<List<InstructionModel>> = _instructionModels
-//
-//    // Function to load data from the repository
-//    // Context should be removed
-//    fun loadInstructionData(context: Context) {
-//        val data = RepositoryProvider.instructionsRepository.getAll(context)
-//        _instructionModels.value = data
-//    }
-//    fun recipeDto.toModel(): RecipeListViewModel {
-//        // Perform the necessary transformation from recipeDto to RecipeListViewModel
-//        return RecipeListViewModel(
-//         //   id = this.id,
-//          //  name = this.name,
-//            //thumbnailUrl = this.thumbnail_url,
-//            // Add other required attributes
-//        )
-//    }
-//}
+package com.tasty.recipesapp.models
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.tasty.recipesapp.dtos.recipeDto
+import com.tasty.recipesapp.providers.RepositoryProvider
+
+class RecipeListViewModel : ViewModel() {
+
+    // LiveData to hold the list of InstructionModels
+    // This should be changed to recipes
+    private val _instructionModels = MutableLiveData<List<InstructionModel>>()
+    val instructionModels: LiveData<List<InstructionModel>> = _instructionModels
+
+    // Function to load data from the repository
+    // Context should be removed
+    fun loadInstructionData(context: Context) {
+        val data = RepositoryProvider.instructionsRepository.getAll(context)
+        _instructionModels.value = data
+    }
+    fun recipeDto.toModel(): RecipeListViewModel {
+        // Perform the necessary transformation from recipeDto to RecipeListViewModel
+        return RecipeListViewModel(
+         //   id = this.id,
+          //  name = this.name,
+            //thumbnailUrl = this.thumbnail_url,
+            // Add other required attributes
+        )
+    }
+}
